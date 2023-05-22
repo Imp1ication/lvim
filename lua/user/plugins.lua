2079f095295da79f2a4cea9a219ed23ce91a52dd
1mplication <1mplicationxwzi@gmail.com>
2023-05-22 22:08:25 +0800

    fix little bug in plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -12,7 +12,6 @@ lvim.plugins = {
     { "zbirenbaum/copilot.lua" },
     {
         "zbirenbaum/copilot-cmp",
-        after = { "copilot.lua" },
         config = function()
             require("copilot_cmp").setup()
         end,
